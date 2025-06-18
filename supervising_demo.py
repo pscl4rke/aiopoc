3344a6a2896fc29a1a5692e743d92a66314a7afc
P. S. Clarke <aiopoc@pscl4rke.net>
2025-06-18 10:07:08 +0100

    Prove out queues as well as simple sleeps

--- supervising_demo.py
+++ supervising_demo.py
@@ -5,15 +5,19 @@ import asyncio
 import supervising
 
 
-async def hello(name: str) -> None:
-    await asyncio.sleep(1)
+async def hello(queue) -> None:
+    name = await queue.get()
     print(f"Hello {name}")
     raise Exception("Went wrong")
 
 
 async def main():
-    task = asyncio.create_task(supervising.supervise_1(hello, "Ebenezer"))
-    await asyncio.sleep(4.5)
+    queue = asyncio.Queue()
+    task = asyncio.create_task(supervising.supervise_1(hello, queue))
+    for name in ["Matthew", "Mark", "Luke", "John"]:
+        await asyncio.sleep(1)
+        await queue.put(name)
+    await asyncio.sleep(1)
     print("Done")
     task.cancel()
 
